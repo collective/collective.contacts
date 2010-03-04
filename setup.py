bdaba2f2eb97a5bd08570a1c68089f631d545de9
frapell <frapell@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-03-04 21:06:46 +0000

    Fixing some files for the tag

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = 'SVN'
+version = '1.4.3'
 
 long_description = (
     read('docs', 'README-en.txt')
