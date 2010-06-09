2f129e78f05fa68232f8dc2e452db32239549633
simu <simu@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-06-09 07:30:11 +0000

    removed no longer needed upgrade step (columns are now handled by the ICustomizableColumns adapters)

--- collective/contacts/upgrades/v1_3_3_to_v1_4_0.py
+++ collective/contacts/upgrades/v1_3_3_to_v1_4_0.py
@@ -2,22 +2,6 @@
 from Products.CMFCore.utils import getToolByName
 from collective.contacts.content.addressbook import AddressBook
 
-def addColumns(portal_setup):
-    """
-    This migration step will include the list of tuples needed to list the
-    columns from the views
-    """
-
-    portal_catalog = getToolByName(portal_setup, 'portal_catalog')
-    address_books = portal_catalog(portal_type='Address Book')
-    organizations_columns = AddressBook.show_on_organizations_view
-    persons_columns = AddressBook.show_on_persons_view
-    
-    for i in address_books:
-        address_book = i.getObject()
-        address_book.show_on_organizations_view = organizations_columns
-        address_book.show_on_persons_view = persons_columns
-
 def updatePersonPhotos(portal_setup):
     """
     This migration step will get all persons, and update their photo field
