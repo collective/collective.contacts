4513dd96be7e36630409ee09b3365949bf8030f8
simu <simu@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-06-09 07:31:29 +0000

    zcml cleanup

--- collective/contacts/browser/find.py
+++ collective/contacts/browser/find.py
@@ -4,7 +4,7 @@ from zope.app.form.browser import DropdownWidget
 from Products.Five.formlib.formbase import PageForm
 
 from collective.contacts import contactsMessageFactory as _
-from collective.contacts.interfaces import IOrganization
+from collective.contacts.interfaces import IPerson, IOrganization
 
 class CustomDropdownWidget(DropdownWidget):
     _displayItemForMissingValue = False
@@ -26,6 +26,28 @@ class FindOrganizationForm(PageForm):
         return super(FindOrganizationForm, self).__call__()
 
     @form.action(_('label_search_organizations', default=u'Search Organizations'))
+    def action_search(self, action, data):
+        pass
+
+    @form.action(_('label_cancel', default=u'Cancel'))
+    def action_search(self, action, data):
+        pass
+
+class FindPersonForm(PageForm):
+    form_fields = form.FormFields(IPerson).omit('workEmail2', 'workEmail3', 'photo', 'organization', 'birthdate')
+    form_fields['country'].custom_widget = CustomDropdownWidgetFactory
+    form_fields['state'].custom_widget = CustomDropdownWidgetFactory
+    label = _('advanced_persons_search', default=u'Advanced persons search:')
+    
+    def __call__(self):
+        self.request.set('disable_border', 1)
+        self.form_fields['firstName'].field.required = False
+        self.form_fields['lastName'].field.required = False
+        # change the form action the point to the search page
+        self.request.set('URL', '%s/search_person' % self.context.absolute_url())
+        return super(FindPersonForm, self).__call__()
+
+    @form.action(_('label_search_persons', default=u'Search Persons'))
     def action_search(self, action, data):
         pass
 
